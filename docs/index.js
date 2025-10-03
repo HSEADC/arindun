ae0eb46b367e19b095ccb6d71f99fc733b7b7819
dunchek <vasilevaevdokia875@gmail.com>
2025-10-03 15:24:49 +0300

    test build & watch

--- docs/index.js
+++ docs/index.js
@@ -1,8 +1,4 @@
 /******/ (() => { // webpackBootstrap
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
 console.log('lol');
 /******/ })()
 ;
-//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiaW5kZXguanMiLCJtYXBwaW5ncyI6Ijs7OztBQUFBQSxPQUFPLENBQUNDLEdBQUcsQ0FBQyxLQUFLLENBQUMsQyIsInNvdXJjZXMiOlsid2VicGFjazovL2FyaW5kdW4vLi9zcmMvaW5kZXguanMiXSwic291cmNlc0NvbnRlbnQiOlsiY29uc29sZS5sb2coJ2xvbCcpXG4iXSwibmFtZXMiOlsiY29uc29sZSIsImxvZyJdLCJzb3VyY2VSb290IjoiIn0=
